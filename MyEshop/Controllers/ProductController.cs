eb55a0d0d85f544bd13154fb31c361fa61313ef4
M.Rozan <maryam.rozanpour@gmail.com>
2020-07-01 17:23:44 +0430

    archive

--- MyEshop/Controllers/ProductController.cs
+++ MyEshop/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http.ModelBinding;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace MyEshop.Controllers
 {
@@ -79,6 +80,49 @@ namespace MyEshop.Controllers
             return PartialView(productComment);
         }
 
+        [Route("Archive")]
+        public ActionResult ArchiveProduct(int pageId = 1, string title = "", int minPrice = 0, int maxPrice = 0, List<int> selectedGroups = null)
+        {
+            ViewBag.Groups = db.ProductGroupRepository.GetAll().ToList();
+            ViewBag.productTitle = title;
+            ViewBag.minPrice = minPrice;
+            ViewBag.maxPrice = maxPrice;
+            ViewBag.pageId = pageId;
+            ViewBag.selectGroup = selectedGroups;
+            List<Products> list = new List<Products>();
+            if (selectedGroups != null && selectedGroups.Any())
+            {
+                foreach (int group in selectedGroups)
+                {
+                    list.AddRange(db.SelectedGroupRepository.GetAll(g => g.GroupID == group).Select(g => g.Products).ToList());
+                }
+                list = list.Distinct().ToList();
+            }
+            else
+            {
+                list.AddRange(db.ProductRepository.GetAll().ToList());
+            }
+
+
+            if (title != "")
+            {
+                list = list.Where(p => p.ProductTitle.Contains(title)).ToList();
+            }
+            if (minPrice > 0)
+            {
+                list = list.Where(p => p.Price >= minPrice).ToList();
+            }
+            if (maxPrice > 0)
+            {
+                list = list.Where(p => p.Price <= maxPrice).ToList();
+            }
+
+            //Pagging
+            int take = 9;
+            int skip = (pageId - 1) * take;
+            ViewBag.PageCount = list.Count() / take;
+            return View(list.OrderByDescending(p => p.CreateDate).Skip(skip).Take(take).ToList());
+        }
     }
 
 }
